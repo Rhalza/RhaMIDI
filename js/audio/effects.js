364bad100a5e31f720861ffb87c9adddaa63dd26
Rhalza <rhalza.dev@gmail.com>
2026-01-17 11:44:34 -0500

    Update effects.js

--- js/audio/effects.js
+++ js/audio/effects.js
@@ -1,38 +1,49 @@
 export const Effects = {
-    createReverb(ctx, seconds = 2, decay = 2, reverse = false) {
-        const rate = ctx.sampleRate;
-        const length = rate * seconds;
-        const impulse = ctx.createBuffer(2, length, rate);
-        const impulseL = impulse.getChannelData(0);
-        const impulseR = impulse.getChannelData(1);
-
-        for (let i = 0; i < length; i++) {
-            const n = reverse ? length - i : i;
-            impulseL[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
-            impulseR[i] = (Math.random() * 2 - 1) * Math.pow(1 - n / length, decay);
-        }
-
+    createReverb(ctx) {
         const convolver = ctx.createConvolver();
-        convolver.buffer = impulse;
-        
         const input = ctx.createGain();
+        const output = ctx.createGain();
         const wet = ctx.createGain();
         const dry = ctx.createGain();
-        const output = ctx.createGain();
+
+        const rate = ctx.sampleRate;
+        const length = rate * 2.0;
+        const impulse = ctx.createBuffer(2, length, rate);
+        const l = impulse.getChannelData(0);
+        const r = impulse.getChannelData(1);
+
+        for (let i = 0; i < length; i++) {
+            const dec = Math.pow(1 - i / length, 2.0);
+            l[i] = (Math.random() * 2 - 1) * dec;
+            r[i] = (Math.random() * 2 - 1) * dec;
+        }
+        convolver.buffer = impulse;
 
         input.connect(dry);
         input.connect(convolver);
         convolver.connect(wet);
-        dry.connect(output);
         wet.connect(output);
+        dry.connect(output);
 
         wet.gain.value = 0.3;
         dry.gain.value = 1.0;
 
-        return { input, output, wet, dry, node: output };
+        return {
+            type: 'reverb',
+            input, output,
+            params: { mix: 0.3, time: 2.0 },
+            nodes: { wet, dry, convolver },
+            set(key, val) {
+                if(key === 'mix') {
+                    this.params.mix = val;
+                    this.nodes.wet.gain.value = val;
+                    this.nodes.dry.gain.value = 1.0 - val;
+                }
+            }
+        };
     },
 
-    createDelay(ctx, time = 0.5, feedbackVal = 0.4) {
+    createDelay(ctx) {
         const delay = ctx.createDelay(5.0);
         const feedback = ctx.createGain();
         const input = ctx.createGain();
@@ -40,18 +51,74 @@ export const Effects = {
         const wet = ctx.createGain();
         const dry = ctx.createGain();
 
-        delay.delayTime.value = time;
-        feedback.gain.value = feedbackVal;
+        delay.delayTime.value = 0.4;
+        feedback.gain.value = 0.3;
 
         input.connect(dry);
         input.connect(delay);
         delay.connect(feedback);
         feedback.connect(delay);
         delay.connect(wet);
-        
-        dry.connect(output);
         wet.connect(output);
+        dry.connect(output);
+        
+        wet.gain.value = 0.5;
 
-        return { input, output, wet, dry, delayNode: delay, feedbackNode: feedback };
+        return {
+            type: 'delay',
+            input, output,
+            params: { time: 0.4, feedback: 0.3, mix: 0.5 },
+            nodes: { delay, feedback, wet, dry },
+            set(key, val) {
+                if(key === 'time') {
+                    this.params.time = val;
+                    this.nodes.delay.delayTime.value = val;
+                } else if (key === 'feedback') {
+                    this.params.feedback = val;
+                    this.nodes.feedback.gain.value = val;
+                } else if (key === 'mix') {
+                    this.params.mix = val;
+                    this.nodes.wet.gain.value = val;
+                    this.nodes.dry.gain.value = 1.0 - val;
+                }
+            }
+        };
+    },
+    
+    createDistortion(ctx) {
+        const shaper = ctx.createWaveShaper();
+        const input = ctx.createGain();
+        const output = ctx.createGain();
+        
+        function makeCurve(amount) {
+            const k = typeof amount === 'number' ? amount : 50;
+            const n_samples = 44100;
+            const curve = new Float32Array(n_samples);
+            const deg = Math.PI / 180;
+            for (let i = 0; i < n_samples; ++i ) {
+                const x = i * 2 / n_samples - 1;
+                curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+            }
+            return curve;
+        }
+
+        shaper.curve = makeCurve(50);
+        shaper.oversample = '4x';
+        
+        input.connect(shaper);
+        shaper.connect(output);
+
+        return {
+            type: 'distortion',
+            input, output,
+            params: { drive: 50 },
+            nodes: { shaper },
+            set(key, val) {
+                if(key === 'drive') {
+                    this.params.drive = val;
+                    this.nodes.shaper.curve = makeCurve(val);
+                }
+            }
+        };
     }
 };
